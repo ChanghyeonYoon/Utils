92b1d487ebab98ab88ee23e5b4653b9864435e09
ChanghyeonYoon <hyeon120411@gmail.com>
2021-09-08 17:53:14 +0900

    useHistoryState.js

---
+++ React/useHistoryState.js
@@ -0,0 +1,30 @@
+import { useState, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+
+/**
+ * 페이지 이동해도 상태 유즈
+ * */
+const useHistoryState = (initialState, key) => {
+    const history = useHistory();
+    const stateValue = history.location.state?.[key];
+
+    const [historyState, setHistoryState] = useState(
+        stateValue === undefined ? initialState : stateValue,
+    );
+
+    const setState = useCallback(
+        (state, replace = false) => {
+            const value = state instanceof Function ? state(historyState) : state;
+
+            setHistoryState(() => value);
+            history.replace({
+                state: replace ? value : { ...history.location.state, [key]: value },
+            });
+        },
+        [history, historyState, key],
+    );
+
+    return [historyState, setState];
+};
+
+export default useHistoryState;
